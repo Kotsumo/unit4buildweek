3143d2263f2d1d00d00d03e1cede569c494739b8
David <davidellis1020@gmail.com>
2021-05-28 10:21:05 -0700

    Added doc for URLs

---
+++ BackendDoc.md
@@ -0,0 +1,3 @@
+URL for database
+
+https://unit4-backend.herokuapp.com/api/auth
--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -10,7 +10,16 @@ const findBy = (filter) => {
     .where(filter).first()
 }
 
+const addUser = (user) => {
+    return db('users')
+    .insert({ 'username': user.username, 'role': user.role, 'item': user.item })
+    .then(res => {
+        return findBy(user.username, user.role)
+    })
+}
+
 module.exports = {
     findBy,
     getAll,
+    addUser,
 }
